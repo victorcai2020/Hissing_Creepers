f91d18bf246f4863b435a7aee3f406188f9d7825
victorcai2020 <62484134+victorcai2020@users.noreply.github.com>
2021-08-15 08:06:54 -0400

    Delete day.py

--- day.py
+++
@@ -1,4 +0,0 @@
-import main as m
-
-class Day:
-    ddraw = ['Animal', 'Wood', 'Stone', 'Iron', 'DIAMONDS!!', 'Bow', "Sticks", 'Villager', 'Go caving', 'Go to the Nether', 'Go to the End', '!!!RAID!!!', 'Night']
